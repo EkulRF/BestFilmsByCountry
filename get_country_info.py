6e02a33961ccfb369e15a2b332e1124f28925b31
EkulRF <87760589+EkulRF@users.noreply.github.com>
2023-12-06 22:30:30 +0000

    Retrieve_LandArea_FromScript

--- get_country_info.py
+++ get_country_info.py
@@ -1,5 +1,4 @@
-
-
+from flask import Flask, render_template
 import pycountry
 from restcountries import RestCountryApiV2 as rca
 
@@ -15,3 +14,22 @@ def get_land_area(iso_code):
             return "Pfft"
     except Exception as e:
         return f"Error: {str(e)}"
+    
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        # Get the list of ISO codes from the form
+        iso_codes = request.form.getlist('iso_codes')
+
+        # Call the Python function for each ISO code and store the results
+        results = {iso_code: get_land_area(iso_code) for iso_code in iso_codes}
+
+        return render_template('index.html', results=results)
+
+    return render_template('index.html', results=None)
+
+if __name__ == '__main__':
+    app.run(debug=True)
